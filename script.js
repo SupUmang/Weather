89626b4ef206b8b261f2c131c725d6fd19af3eeb
Colossal-Dev <chaurasiyav198@gmail.com>
2024-11-07 13:18:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function getInputValue() {
+  const inputValue = document.querySelector(".input-city-name").value;
+  console.log(inputValue);
+  fetch(
+    `http://api.weatherapi.com/v1/current.json?key=543974d8866f45c48a3183737242910&q=${inputValue}`
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error));
+}
